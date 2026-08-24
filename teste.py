b20260afd9271bad4adb1dfe8f2eadb3cff74931
Ryan Almeida <ryanalds21@gmail.com>
2026-08-24 13:43:35 -0300

    DataFrame e Series

---
+++ teste.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "Name" : [
+             "Braund, Mr. Owen Harris",
+            "Allen, Mr. William Henry",
+            "Bonnell, Miss Elizabeth",
+        ],
+        "Age" : [22,35,58],
+        "Sex" : ["Male", "Male", "Female"],
+    }
+)
+
+df["Active"] =  True
+
+print(df)
