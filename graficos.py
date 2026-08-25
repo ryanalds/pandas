5ff61d48a58032f364e8ab75e3e6bfe763fec995
Ryan <ryanalds21@gmail.com>
2026-08-25 10:10:41 -0300

    filtragem e estilização dos graficos

--- graficos.py
+++ graficos.py
@@ -6,5 +6,13 @@ air_quality = pd.read_csv('data/air_quality_no2.csv', index_col=0, parse_dates=T
 # air_quality.plot()
 # plt.show() #cria grafico de linhas por padrão
 
-air_quality["station_paris"].plot()
+# air_quality["station_antwerp"].plot() #filtrando a coluna, o plot em line é o padrão 
+
+# air_quality.plot.scatter(x="station_london", y="station_paris", alpha=0.5) # passando qual coluna será o eixo x e y, alpha controla a opacidade do ponto
+
+# air_quality.plot.box()
+
+axs = air_quality.plot.area(figsize=(12,4),xlabel="Date Time",subplots=True) # grafico de area
+
+plt.savefig("data/grafico.png")
 plt.show()
--- subconj.py
+++ subconj.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 titanic = pd.read_csv("data/titanic.csv")
 
-# print(titanic.head())
+print(titanic.head())
 
 # filtrar por idade
 # age = titanic["Age"]
@@ -25,5 +25,5 @@ titanic = pd.read_csv("data/titanic.csv")
 # adult_names = titanic.loc[titanic["Age"] > 35, "Name"] #Ao usar loc, a parte antes da vírgula representa as linhas desejadas e a parte depois da vírgula representa as colunas que você deseja selecionar
 # print(adult_names.head)
 
-f = titanic.iloc[9:25, 2:6] #Quando estiver especificamente interessado em determinadas linhas e/ou colunas com base em sua posição na tabela, use o iloc
-print(f)
+# f = titanic.iloc[9:25, 2:6] #Quando estiver especificamente interessado em determinadas linhas e/ou colunas com base em sua posição na tabela, use o iloc
+# print(f)
